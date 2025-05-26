11b09bf9a4aa33cd7000200cabd90bc5470d052d
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2025-05-25 23:18:17 -0500

    cambio de version a 3.9

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, jsonify
+import joblib
+import numpy as np
+import os
+
+# Ruta base del proyecto
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Cargar los modelos
+model = joblib.load(os.path.join(BASE_DIR, 'model', 'modelo_brasil_solar.pkl'))
+scaler = joblib.load(os.path.join(BASE_DIR, 'model', 'modelo_brasil_scaler.pkl'))
+pca = joblib.load(os.path.join(BASE_DIR, 'model', 'modelo_brasil_pca.pkl'))
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('home.html')
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route ('/modelo', methods=['GET', 'POST'])
+def modelo():
+    return render_template('modelo.html')
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    try:
+        #Obtenemos los datos de entrada en formato JSON
+        data = request.get_json(force=True)
+        input_data = np.array(data['features']) #una lista numerica
+
+        #Preprocesamiento
+        input_scaled = scaler.transform(input_data)
+        input_pca = pca.transform(input_scaled)
+
+        #Predicción
+        prediction = model.predict(input_pca)
+        return jsonify({'predicción': float(prediction[0])})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
